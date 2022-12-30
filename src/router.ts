1d2edcac3531f62c7439e562016712e2da42d670
Rotela Domingo Ramon <74286462+rotela5@users.noreply.github.com>
2022-12-30 04:32:43 -0300

    refactoring method

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import getRoutes from "./routes/get";
 import postRoutes from "./routes/post";
+import putRoutes from "./routes/post";
+import deleteRoutes from "./routes/post";
 
 const router = Router();
 
@@ -16,12 +18,12 @@ router.post("/publication", postRoutes.publication);
 router.post("/user", postRoutes.user);
 router.post("/habit", postRoutes.habit);
 
-router.put("/publication", postRoutes.publication);
-router.put("/user", postRoutes.user);
-router.put("/habit", postRoutes.habit);
+router.put("/publication", putRoutes.publication);
+router.put("/user", putRoutes.user);
+router.put("/habit", putRoutes.habit);
 
-router.delete("/publication", postRoutes.publication);
-router.delete("/user", postRoutes.user);
-router.delete("/habit", postRoutes.habit);
+router.delete("/publication", deleteRoutes.publication);
+router.delete("/user", deleteRoutes.user);
+router.delete("/habit", deleteRoutes.habit);
 
 export default router;
